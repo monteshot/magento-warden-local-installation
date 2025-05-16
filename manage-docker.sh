b0f9601559c88af519abe45c1faad737ba3341e4
monteshot <faqreg@gmail.com>
2025-05-16 19:24:41 +0300

    Fix Docker installation script to exit gracefully on curl failure

--- manage-docker.sh
+++ manage-docker.sh
@@ -2,7 +2,7 @@
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do echo $PASSOWRD | sudo -S sshpass apt-get remove $pkg; done
 
-curl -fsSL https://get.docker.com -o get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh || exit 0;
 echo $PASSOWRD | sudo -S sshpass sh ./get-docker.sh
 
 echo $PASSOWRD | sudo -S sshpass usermod -aG docker $USER;
