ae315f35e3e99e14a779461df897dd82d1e56883
monteshot <faqreg@gmail.com>
2025-05-17 10:46:11 +0000

    Quote MAGENTO_CONTENT_PATH in mv and rm commands for improved path handling

--- manage-magento.sh
+++ manage-magento.sh
@@ -7,13 +7,13 @@ cd $MAGENTO_CONTENT_PATH
 warden env exec -e COMPOSER_AUTH="$AUTH_JSON" php-fpm composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION /var/www/html/tmp/ -vvv
 
 echo "$PASSWORD" | sudo -S rm -rf var/
-mv $MAGENTO_CONTENT_PATH/tmp/* $MAGENTO_CONTENT_PATH/
-rm -rf $MAGENTO_CONTENT_PATH/tmp/
+echo "$PASSWORD" | sudo -S mv "$MAGENTO_CONTENT_PATH"/tmp/* "$MAGENTO_CONTENT_PATH"/
+echo "$PASSWORD" | sudo -S rm -rf "$MAGENTO_CONTENT_PATH"/tmp/
 
 warden env exec php-fpm rm ./app/etc/env.php;
 warden env exec php-fpm rm ./app/etc/config.php;
 warden env exec php-fpm rm -rf ./var/cache/* ./var/page_cache/* ./var/view_preprocessed/* ./generated/code/* ./pub/static/*;
-main_domain=$MAIN_DOMAIN
+
 cp "$(pwd)/magento" "$MAGENTO_CONTENT_PATH"
 
 warden env exec -e main_domain="$MAIN_DOMAIN" php-fpm manage-magento-inside.sh
