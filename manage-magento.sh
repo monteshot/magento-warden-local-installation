a96e14f589ff3e161ba1f09d797f7ba1c21adf68
monteshot <faqreg@gmail.com>
2025-05-19 20:33:43 +0000

    fix: update Magento installation path and cleanup process in manage-magento.sh

--- manage-magento.sh
+++ manage-magento.sh
@@ -5,7 +5,9 @@ AUTH_JSON="$(cat "$MAIN_SCRIPT_DIR"/magento/auth.json)"
 cd $MAGENTO_CONTENT_PATH
 
 echo "$PASSWORD" | sudo -S rm -rf "$MAGENTO_CONTENT_PATH/*"
-warden env exec -e COMPOSER_AUTH="$AUTH_JSON" php-fpm composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION /var/www/html/tmp/ -vvv
+
+warden env exec php-fpm mkdir -p /tmp/magento
+warden env exec -e COMPOSER_AUTH="$AUTH_JSON" php-fpm composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION /tmp/magento -vvv
 
 if [ $? -ne 0 ]; then
     echo "Failed to fetch Magento installation files. Please check the logs above."
@@ -13,10 +15,10 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Cleaning up old files..."
-echo "$PASSWORD" | sudo -S rm -rf ./app/ ./bin/ ./dev/ ./generated/ ./lib/ ./phpserver/ ./pub/ ./setup/ ./var/ ./vendor/
+echo "$PASSWORD" | sudo -S rm -rf ./*
 
 echo "Moving new files..."
-echo "$PASSWORD" | sudo -S mv -f "$MAGENTO_CONTENT_PATH"/tmp/* "$MAGENTO_CONTENT_PATH"/
+echo "$PASSWORD" | sudo -S mv -f "/tmp/magento/*" "$MAGENTO_CONTENT_PATH/"
 echo "Cleaning up temporary files..."
 echo "$PASSWORD" | sudo -S rm -rf "$MAGENTO_CONTENT_PATH"/tmp/
 
