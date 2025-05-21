9225c84d2206bc7dab2960f5aaa1fc154a1f5e4b
monteshot <faqreg@gmail.com>
2025-05-21 14:46:52 +0000

    Script fixes

--- manage-warden.sh
+++ manage-warden.sh
@@ -20,7 +20,7 @@ sed -i s/WARDEN_SERVICE_DOMAIN=$clear_url//g $HOME_DIR/.warden/.env
 echo "WARDEN_SERVICE_DOMAIN=$clear_url" | tee -a $HOME_DIR/.warden/.env
 sed -i s/TRAEFIK_LISTEN=127.0.0.1//g $HOME_DIR/.warden/.env
 echo "TRAEFIK_LISTEN=0.0.0.0" | tee -a $HOME_DIR/.warden/.env
-sudo ls -lah $HOME_DIR/.warden
+echo "$PASSWORD" | sudo -S ls -lah $HOME_DIR/.warden
 echo "Setting up Warden user group and user ID inside container"
 warden env exec php-fpm sudo groupmod -g 33333 www-data
 warden env exec php-fpm sudo usermod -u 33333 -g 33333 www-data
