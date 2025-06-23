ff3694b902270b9ca87b69605a321729dc5e6941
monteshot <faqreg@gmail.com>
2025-06-23 17:20:47 +0300

    Update manage-warden.sh to clone Warden repository and adjust environment variables

--- manage-warden.sh
+++ manage-warden.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 
-NONINTERACTIVE=1 brew install wardenenv/warden/warden -q;
+#NONINTERACTIVE=1 brew install wardenenv/warden/warden -q;
+
+sudo mkdir /opt/warden
+sudo chown $(whoami) /opt/warden
+git clone -b main https://github.com/wardenenv/warden.git /opt/warden
+echo 'export PATH="/opt/warden/bin:$PATH"' >> ~/.bashrc
+PATH="/opt/warden/bin:$PATH"
+warden svc up
 
 echo "Creating Warden-related files(containers) under home directory"
 warden svc up;
@@ -13,10 +20,16 @@ cd $MAGENTO_CONTENT_PATH
 warden env up --remove-orphans
 
 echo "Setting up Warden"
+
 sed -i s/WARDEN_SERVICE_DOMAIN=$MAIN_DOMAIN//g $HOME_DIR/.warden/.env
 echo "WARDEN_SERVICE_DOMAIN=$MAIN_DOMAIN" | tee -a $HOME_DIR/.warden/.env
+
 sed -i s/TRAEFIK_LISTEN=127.0.0.1//g $HOME_DIR/.warden/.env
 echo "TRAEFIK_LISTEN=0.0.0.0" | tee -a $HOME_DIR/.warden/.env
+
+sed -i s/WARDEN_MUTAGEN_ENABLE=1//g $HOME_DIR/.warden/.env
+echo "WARDEN_MUTAGEN_ENABLE=0" | tee -a $HOME_DIR/.warden/.env
+
 echo "$PASSWORD" | sudo -S ls -lah $HOME_DIR/.warden
 echo "Restarting Warden to apply changes"
 warden env down
